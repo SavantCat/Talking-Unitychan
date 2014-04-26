b55fb92d11cf3448a10fb770b541ef8e44208a22
Savant_Cat <scanf@me.com>
2014-04-26 15:16:16 +0900

    update

---
+++ Assets/rotato.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotato : MonoBehaviour {
+
+	public Vector3 r = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(r);
+	}
+}
